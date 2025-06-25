1a3b75a5f21ff5a09221edfb0b2ee7e6d10f9b04
Navneet Singh Ghura <64772740+Navneet-Singh-Ghura@users.noreply.github.com>
2025-06-24 17:08:21 -0700

    updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+# initialize git, letting git know to manage the folder
 git init
+
+#add all the files 
 git add .
 git commit -m "Load project to Github from local"
 
+# create repo in github and run below command
 git remote add origin https://github.com/Navneet-Singh-Ghura/pysparkproject.git
+
+#check origin 
 git remote -v
+
+#check branch name 
 git branch 
+
+#change branch name 
 git branch -M main
+
+#sync files in github to local
 git rebase origin main
+
+#push local files to github
 git push --set-upstream origin main
